9ba45fd0fa8785739edc93415dfa6bf5a3fd8d34
zaq12wsx4 <jonathanfinn11@gmail.com>
2018-10-22 19:24:21 -0500

    Itterates through cards in each image file

--- SpellSplitter.py
+++ SpellSplitter.py
@@ -13,12 +13,23 @@ if images[0] == ".DS_Store":
 
 # Do the following for each image
 for image in images:
-    single_card = (20, 20, 520, 720) # (left, upper, right, lower)
+    single_card = [20, 20, 520, 720] # (left, upper, right, lower)
+    new_cards = []
+    for count in range(9):
+        column = int(count/3)
+        row = count % 3
+        new_cards.append(((single_card[0] + row*500), (single_card[1] + column*700), (single_card[2] + row*500), (single_card[3] + column*700)))
+
+    print(new_cards)
     spell_page = ("./"+folder+"/"+image)
     print(spell_page)
 
     spell_page = Image.open(spell_page)
-    single_card = spell_page.crop(single_card)
+
+    card_images = []
+    for i in range(len((new_cards))):
+        card_images.append(spell_page.crop(new_cards[i]))
+        card_images[i].show()
 
     # spell_page.show()
-    single_card.show()
+    # single_card.show()
